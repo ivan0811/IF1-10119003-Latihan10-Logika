7607473c020a012f33770b8a6a9879c91b54231c
ivan0811 <ivanfaathirza@gmail.com>
2020-10-16 18:20:44 +0700

    initial commit

---
+++ src/com/logika/Logika.java
@@ -0,0 +1,19 @@
+package com.logika;
+
+/*
+	NAMA = Ivan Faathirza
+	KELAS = IF1
+	NIM = 10119003
+	Deskripsi program = program ini untuk menampilkan logika true atau false
+ */
+public class Logika {
+
+    public static void main(String[] args) {
+	    int umur = 19;
+	    boolean dewasa;
+
+	    //true or false
+	    dewasa = (umur > 7);
+	    System.out.println("Status Dewasa : "+dewasa); // true
+    }
+}
